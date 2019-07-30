408e785d9dbaf8d4624620922eef7ebab7e3215c
southparkstan123 <southparkstan123@gmail.com>
2019-07-30 20:29:48 +0800

    Add api configs

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+    DOMAIN: "https://itunes.apple.com",
+    HUNDRED_FREE_APPS_API: '/hk/rss/topfreeapplications/limit=100/json',
+    RECOMMENDED_APPS_API: '/hk/rss/topgrossingapplications/limit=10/json',
+    LOOKUP_API: '/hk/lookup'
+}
