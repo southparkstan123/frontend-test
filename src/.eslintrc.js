4898ff03a13cf99370b5a76ea11828ceaf253ef6
southparkstan123 <southparkstan123@gmail.com>
2019-07-31 23:49:05 +0800

    config eslint

---
+++ src/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "react-app",
+    "rules" : {
+        "indent": ["error", 2]
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true,
+        }
+    }
+}
