bf98fd04069e76d9129541b7b823de95b98617d4
southparkstan123 <southparkstan123@gmail.com>
2019-08-02 00:59:13 +0800

    minor fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 //@flow
 export default {
-    DOMAIN: "https://itunes.apple.com",
+    DOMAIN: 'https://itunes.apple.com',
     HUNDRED_FREE_APPS_API: '/hk/rss/topfreeapplications/limit=100/json',
     RECOMMENDED_APPS_API: '/hk/rss/topgrossingapplications/limit=10/json',
     LOOKUP_API: '/hk/lookup'
