ef392d9127c41996d256cc7204de1599cfb4a45f
southparkstan123 <southparkstan123@gmail.com>
2019-08-09 00:56:46 +0800

    Fix SearchBar

--- src/SearchBar/components/SearchBar.js
+++ src/SearchBar/components/SearchBar.js
@@ -1,13 +1,8 @@
 // @flow
-import type { SearchBarProps, RootState } from '../../types';
+import type { SearchBarProps } from '../../types';
 import React from 'react';
-import { useMappedState } from 'redux-react-hook';
 
 export default function SearchBar(props: SearchBarProps) {
-
-    const isAppLoading: boolean = useMappedState((state: RootState) => {
-        return state.AppListReducer.isAppLoading;
-    });
 
     return (
         <nav id="search-bar" className="sticky-top navbar-light bg-light p-2">
@@ -16,8 +11,7 @@ export default function SearchBar(props: SearchBarProps) {
                     id="search-input" 
                     onChange={(event: SyntheticInputEvent<HTMLInputElement>) => props.onChangeKeyword(event.target.value.trim())} 
                     className="text-center form-control mx-5" 
-                    type="text" 
-                    disabled={isAppLoading}
+                    type="text"
                     placeholder='搜尋' />
             </div>
         </nav>
