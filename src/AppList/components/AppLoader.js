1007cffcc6a0c1ca792be965ac568dbaebd8c12d
southparkstan123 <southparkstan123@gmail.com>
2019-08-09 00:24:44 +0800

    Modify AppLoader

--- src/AppList/components/AppLoader.js
+++ src/AppList/components/AppLoader.js
@@ -1,7 +1,9 @@
+// @flow
 import React from 'react';
 import { useMappedState } from 'redux-react-hook';
 import LoadingSpinner from '../../LoadingSpinner/components/LoadingSpinner';
-import { faFan } from '@fortawesome/free-solid-svg-icons';
+import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import type { RootState } from '../../types';
 
 export default function AppLoader() {
 
@@ -12,7 +14,7 @@ export default function AppLoader() {
     return (
         (isAppLoading) ? 
             <div className='row my-5 justify-content-center'>
-                <LoadingSpinner icon={faFan} size="3x" spin={true} isfullscreen={false}></LoadingSpinner>
+                <LoadingSpinner icon={faSyncAlt} size="3x" spin={true} isfullscreen={false}></LoadingSpinner>
             </div> : false
     )
 }
